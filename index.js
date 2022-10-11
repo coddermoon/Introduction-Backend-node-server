bb9865ea95b12d1b2045e6695372f64f866b8499
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-10-12 02:16:12 +0600

    added phone data in backend

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require('express')
 const app = express()
 const port= 5000;
 
+const phones = require('./phones.json')
+
 app.get('/',(req,res)=>{
 res.send('hello World from server')
 
 })
 
+app.get('/phones',(req,res)=>{
+    res.send(phones)
+})
+app.get('/phones/:id',(req,res)=>{
+    const id = parseInt(req.params.id)
+const phone = phones.find(ph=>ph.id===id  )|| {}
+res.send(phone) 
+})
 app.listen(port,()=>{
     console.log('server running on port :',port)
 })
